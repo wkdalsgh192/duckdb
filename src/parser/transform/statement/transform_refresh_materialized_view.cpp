ddff008d873a18e372d6770cde2bba7b7db4e0c5
Minho <deakse2@gmail.com>
2024-12-09 17:48:03 -0800

    feat:add refresh materialized view to transformer

---
+++ src/parser/transform/statement/transform_refresh_materialized_view.cpp
@@ -0,0 +1,19 @@
+#include "duckdb/parser/parsed_data/create_table_info.hpp"
+#include "duckdb/parser/statement/create_statement.hpp"
+#include "duckdb/parser/transformer.hpp"
+
+namespace duckdb {
+
+unique_ptr<CreateStatement>
+Transformer::TransformRefreshMaterializedView(duckdb_libpgquery::PGRefreshMatViewStmt &stmt) {
+
+	auto result = make_uniq<CreateStatement>();
+	auto info = make_uniq<CreateTableInfo>();
+
+	info->table = stmt.relation->relname;
+
+	result->info = std::move(info);
+	return result;
+}
+
+} // namespace duckdb
